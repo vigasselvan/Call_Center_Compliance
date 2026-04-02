85fe646048964738c9e44a7a9da9de9018e4994a
Vigas <77221759+vigasselvan@users.noreply.github.com>
2026-04-02 11:38:18 +0530

    scaffold client folder of the website
    scaffold basic client files and folders of the website

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        App.JS
+      </section>
+    </>
+  )
+}
+
+export default App
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/record", records);
 
 // start the Express server
 app.listen(PORT, () => {
